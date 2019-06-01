01f81a0e1b8e9ca9f3e6cd8241c5bcd9116bb279
Nishi Sheth <nishivsheth@gmail.com>
2019-06-01 11:08:10 +1000

    added ThreadHTTPServer class

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -10,6 +10,9 @@ from urllib.parse import unquote, parse_qs
 import threading
 from socketserver import ThreadingMixIn
 
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+    
 memory = {}
 
 form = '''<!DOCTYPE html>
